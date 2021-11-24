46666a4eff2ebbb44f889318715ab30b2ca3e006
balderarrouas <balder.arrouas@gmail.com>
2021-11-24 15:01:30 +0100

    Har startet liste

---
+++ js/fetch/fetchMovies.js
@@ -0,0 +1,26 @@
+const out = (str) => console.log(str);
+
+
+const getMovieURL =  "http://localhost:8080/movie/get";
+
+
+async function getMovies(){
+    return fetch(getMovieURL).then(response => response.json());
+}
+
+async function callGetMovies(){
+    const promise = getMovies();
+    await promise.then(movieMapper);
+}
+
+let movieMap = new Map();
+
+function movieMapper(herroPlsDoYouLikeThisNameJonasOrDoYouDont){
+    herroPlsDoYouLikeThisNameJonasOrDoYouDont.forEach(movie =>{
+        movieMap.set(movie.movieId, movie)
+    })
+}
+
+async function getAll(){
+    await callGetMovies();
+}
