347246e33fc1b6cdbf83cf05d6a5f3f51696be7f
Frederik-S89 <f-storgaard@hotmail.com>
2021-12-03 10:39:48 +0100

    Booking, Jonas lugter af ged og gedeost3

--- js/CustomerBooking.js
+++ js/CustomerBooking.js
@@ -24,19 +24,21 @@ let postRequestCustomerBooking = {
 
 let bookingJson = {
     "customerEmail": "",
-    "customerNumber": ""
+    "customerNumber": "",
+    "customerMovie": ""
 
 }
 
 function createCustomerBooking() {
     let inpValue1 = document.getElementById("customerEmail");
     let inpValue2 = document.getElementById("customerNumber");
+    let inpValue3 = document.getElementById("customerMovie");
 
     console.log(inpValue1);
     console.log(inpValue1.value);
     bookingJson.customerEmail = inpValue1.value;
     bookingJson.customerNumber = inpValue2.value;
-
+    bookingJson.customerMovie = inpValue3.value;
     postRequestCustomerBooking.body = JSON.stringify(bookingJson)
     fetch(saveBookURL, postRequestCustomerBooking).catch((error) => console.log(error));
 }
